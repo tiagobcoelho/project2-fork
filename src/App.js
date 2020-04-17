17035ff8b6372dd0eb5646646e9ebadd3087ac66
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-17 10:13:02 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/my-profile">MyProfile</Link>
+            </li>
+            <li>
+              <Link to="/user-profile/janedoe">Jane Doe Profile</Link>
+            </li>
+            <li>
+              <Link to="/user-profile/johndoe">John Doe Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/my-profile">
+            <MyProfile />
+          </Route>
+          <Route path="/user-profile/:githubLogin" component={UserProfile} />
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
