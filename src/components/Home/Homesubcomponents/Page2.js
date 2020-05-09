58d47d8f1932d34101b1abba9bb309b12bc8204d
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-05-09 20:25:07 +0100

    clean up

--- src/components/Home/Homesubcomponents/Page2.js
+++ src/components/Home/Homesubcomponents/Page2.js
@@ -1,4 +1,4 @@
-import React,{ useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from 'react-icons/io';
 import { GoPrimitiveDot } from 'react-icons/go';
